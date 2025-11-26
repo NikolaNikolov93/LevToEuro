f06c84b36ac26073b14c5e7002d7589da0a6d0b2
Nikola <niksnikolov93@gmail.com>
2025-11-26 09:25:14 +0200

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ const handleTotalPrice = (value: string) => {
         onChange={(e) => handleGivenMoney(e.target.value)}
         type="text"
         inputMode="numeric"
-        step="0.01"
+        pattern="[0-9]"
       />
 
       <input
@@ -54,7 +54,7 @@ const handleTotalPrice = (value: string) => {
         onChange={(e) => handleTotalPrice(e.target.value)}
         type="text"
         inputMode="numeric"
-        step="0.01"
+        pattern="[0-9]"
       />
 
       <div className={styles.resultBox}>
